25c3afbd46ede0b994d001c856911c16b63327f3
petersenjoern <petersenjoern>
2021-07-18 21:59:21 +0200

    ctgov db and extraction running

--- ctgov.sh
+++ ctgov.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
 MY_PATH="`dirname \"$0\"`"
-DIR="${MY_PATH}/data/ctgov" 
-DB_NAME=ctgov
+DIR_DOWNLOAD="${MY_PATH}/data/ctgov/download"
+DIR_PSQL_EXTRACTION="${MY_PATH}/data/ctgov/extraction"
 
 #20210601 or more general: yyyymm01
 DATE=$1
 CTGOV_FILE_NAME="${DATE}_clinical_trials.zip"
 
-mkdir -p "$DIR"
+mkdir -p "$DIR_DOWNLOAD"
+mkdir -p "$DIR_PSQL_EXTRACTION"
 
-# if ! wget -P "$DIR" "https://aact.ctti-clinicaltrials.org/static/static_db_copies/monthly/$CTGOV_FILE_NAME"
-# then
-#   rm -f "$DIR/$CTGOV_FILE_NAME"
-#   echo "Download failed."
-#   exit 1
-# fi
+if ! wget -P "$DIR_DOWNLOAD" "https://aact.ctti-clinicaltrials.org/static/static_db_copies/monthly/$CTGOV_FILE_NAME"
+then
+  rm -f "$DIR_DOWNLOAD/$CTGOV_FILE_NAME"
+  echo "Download failed."
+  exit 1
+fi
 
-# unzip -o -d "$DIR" "$DIR/$CTGOV_FILE_NAME"
+unzip -o -d "$DIR_DOWNLOAD" "$DIR_DOWNLOAD/$CTGOV_FILE_NAME"
 set -e
 
-echo "Starting docker-compose postgres db"
-docker-compose --env-file ".env" up
+echo "Starting docker-compose postgres db and extract data with scripts/ctgov/01-init.sh"
+docker-compose --env-file ".env"
---
+++ scripts/ctgov/01-init.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+set -e
+export PGPASSWORD=$POSTGRES_PASSWORD;
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DB TO $POSTGRES_USER;
+EOSQL
+
+dropdb -U "$POSTGRES_USER" --if-exists "$POSTGRES_DB"
+createdb -U "$POSTGRES_USER" "$POSTGRES_DB"
+
+pg_restore -U "$POSTGRES_USER" -e -v -O -x --dbname="$POSTGRES_DB" "/data/ctgov/download/postgres_data.dmp"
+psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "ALTER ROLE $POSTGRES_USER SET search_path TO ctgov,public;"
+
+# Check the download:
+YEAR=$(date +'%Y')
+echo "Year: $YEAR"
+
+QUERY="
+SELECT
+    t1.overall_status status,
+    COUNT(1) study_count,
+    SUM(t1.enrollment) enrollment
+FROM (
+    SELECT
+        nct_id,
+        date_part('year', start_date) AS start_year,
+        date_part('year', completion_date) AS completion_year,
+        enrollment,
+        overall_status,
+        study_type
+    FROM studies
+    WHERE
+        enrollment < 50000
+) t1
+JOIN calculated_values t2
+    ON t1.nct_id = t2.nct_id
+WHERE
+    t2.has_us_facility
+    AND t1.start_year <= $YEAR
+    AND (
+        t1.completion_year >= $YEAR
+        OR t1.completion_year IS NULL
+    )
+    AND t1.overall_status = ANY (
+        ARRAY[
+            'Recruiting',
+            'Active, not recruiting',
+            'Not yet recruiting',
+            'Enrolling by invitation'
+        ]
+    )
+    AND t1.study_type = 'Interventional'
+GROUP BY
+    t1.overall_status
+ORDER BY
+    3 DESC
+"
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "\copy (${QUERY}) to '/data/ctgov/extraction/test22.csv' with csv"
